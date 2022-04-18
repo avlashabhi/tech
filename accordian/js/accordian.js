b710018e401ab5868cff5c50fba2aa071af63cb9
acer <abhilash.a@techversantinfo.com>
2022-04-18 23:49:26 +0530

    final accordian

--- accordian/js/accordian.js
+++ accordian/js/accordian.js
@@ -1,12 +1,8 @@
-console.log("Abhi");
 $(document).ready(function () {
     $(".musiccontent").click(function () {
-        // if ($(".musiccontent").sibilings("p").hasClass("musicp")) {
-        $(".musicp").slideDown("slow");
-        $(".fa-solid").removeClass("fa-angle-down").addClass("fa-angle-up");
+        $(".musicp").slideUp();
+        $(this).next(".musicp").slideDown();
+        $(this).children().removeClass("fa-angle-down").addClass("fa-angle-up");
         $(".musiccontent").addClass("extra");
-        // } else {
-
-        // }
     });
 });
