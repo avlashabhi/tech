f195c2cb9ade2d165ee0364747a773af620fe220
acer <abhilash.a@techversantinfo.com>
2022-04-19 15:36:18 +0530

    final

--- accordian/js/accordian.js
+++ accordian/js/accordian.js
@@ -1,13 +1,15 @@
 $(document).ready(function () {
     $(".musiccontent").click(function () {
         $(".musicp").slideUp();
-        $(this).next(".musicp").slideDown();
+        $(this).next(".musicp").stop(true).slideDown();
         $(this).children().removeClass("fa-angle-down").addClass("fa-angle-up");
         $(".musiccontent").addClass("extra");
-
+        // $(".musiccontent").children().removeClass("fa-angle-down").addClass("fa-angle-up");
     });
+
+
     // $(".musiccontent").dblclick(function () {
-    //     $(".musiccontent").next(".musicp").slideUp();
+    //     $(".musiccontent").next(".musicp").stop(true).slideUp();
     //     $(".musiccontent").removeClass("extra");
     //     $(".musiccontent").children().removeClass("fa-angle-up").addClass("fa-angle-down");
     // });
