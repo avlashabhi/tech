c927f157cad7d3caa2802cbed0afde828dbe6f6d
acer <abhilash.a@techversantinfo.com>
2022-05-05 22:48:30 +0530

    node basics

---
+++ studynodejs/script.js
@@ -0,0 +1,67 @@
+/*console.log("running");
+const array = ["a", "b", "c"];
+console.log(array);
+const newArray = [...array, "e"];
+console.log(newArray);
+const check = [array, "e"];
+console.log(check, array);*/
+
+/*setInterval(() => {
+    console.log("running");
+
+}, 1000);*/
+/*const interval = setInterval(() => {
+    console.log("running");
+
+}, 1000);*/
+
+
+/*setTimeout(() => {
+    console.log("running");
+
+}, 1000);*/
+
+
+/*const interval = setInterval(() => {
+    console.log("running");
+
+}, 1000);
+setTimeout(() => {
+    clearInterval(interval)
+
+}, 3000);*/
+
+// console.log(global);
+
+/*console.log(__dirname);
+console.log(__filename);*/
+
+/*const path = require("path");
+// console.log(path);
+console.log(path.basename(__filename));
+console.log(path.extname(__filename));
+console.log(path.parse(__filename));*/
+
+/*const path = require("path");
+console.log(path.join(__dirname, "apifolder", "script.js"));*/
+
+const fs = require("fs");
+const path = require("path");
+// console.log(fs);
+/*fs.mkdir(path.join(__dirname, "/api"), {}, (err) => {
+    if (err) throw err;
+});*/
+/*fs.mkdir(path.join(__dirname, "/api/api1"), {}, (err) => {
+    if (err) throw err;
+});*/
+/*fs.rmdir(path.join(__dirname, "api/api1"), {}, (err) => {
+    if (err) throw err;
+});*/
+/*fs.mkdir(path.join(__dirname, "/api/api1"), {}, (err) => {
+    if (err) throw err;
+});*/
+/*fs.rmdir(path.join(__dirname, "api"), {
+    recursive: true
+}, (err) => {
+    if (err) throw err;
+});*/
